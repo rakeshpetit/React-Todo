2a59f720376fb93f85ebca20a331deccb65b42d8
Rakesh <rakeshpetit@gmail.com>
2017-06-17 18:42:42 +0530

    With boilerplate

---
+++ app/app.jsx
@@ -0,0 +1,13 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var {Route, Router, IndexRoute, hashHistory} = require('react-router');
+
+var TodoApp = require('TodoApp');
+$(document).foundation();
+
+require('style!css!sass!applicationStyles')
+
+ReactDOM.render(
+<TodoApp/>,
+  document.getElementById('app')
+);
