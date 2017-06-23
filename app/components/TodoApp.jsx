d2be7f7022be5b9926fcfca84c63c4c50a24ec30
Rakesh <rakeshpetit@gmail.com>
2017-06-24 00:03:39 +0530

    Added testing components

--- app/components/TodoApp.jsx
+++ app/components/TodoApp.jsx
@@ -1,10 +1,31 @@
 var React = require('react');
+var TodoList = require('TodoList');
 
 var TodoApp = React.createClass({
+  getInitialState: function() {
+    return {
+      todos: [
+        {
+          id: 1,
+          text: 'Walk in the park'
+        }, {
+          id: 2,
+          text: 'Play with Eashwar'
+        }, {
+          id: 3,
+          text: 'Exercise daily'
+        }, {
+          id: 4,
+          text: 'Job hunt'
+        }
+      ]
+    }
+  },
   render: function () {
+    var {todos} = this.state;
     return (
       <div>
-        TodoApp
+        <TodoList todos={todos}/>
       </div>
     )
   }
---
+++ app/tests/components/TodoList.test.jsx
@@ -0,0 +1,30 @@
+var React = require('react');
+var ReactDOM = require('react-dom');
+var expect = require('expect');
+var $ = require('jQuery');
+var TestUtils = require('react-addons-test-utils');
+
+var TodoList = require('TodoList');
+var Todo = require('Todo');
+
+describe('TodoList',() => {
+
+  it('should exist',()=> {
+    expect(TodoList).toExist();
+  });
+
+  it('should render one Todo component for each todo item',()=> {
+    var todos = [{
+      id: 1,
+      text: 'First Item'
+    }, {
+      id: 2,
+      text: 'Second item'
+    }];
+    var todoList = TestUtils.renderIntoDocument(<TodoList todos={todos}/>);
+    var todoComponents = TestUtils.scryRenderedComponentsWithType(todoList,Todo);
+    expect(todoComponents.length).toBe(todos.length);
+  });
+
+
+});
