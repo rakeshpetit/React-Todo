378e87ac8548bca65cb7ba15d0944e81f5c86ec3
Rakesh <rakeshpetit@gmail.com>
2017-06-26 11:35:41 +0530

    Search function

--- app/api/TodoAPI.jsx
+++ app/api/TodoAPI.jsx
@@ -18,5 +18,31 @@ module.exports = {
 
     }
     return $.isArray(todos) ? todos : [];
+  },
+  filterTodos: function (todos, showCompleted, searchText) {
+    var filterTodos = todos;
+
+    filterTodos = filterTodos.filter((todo) => {
+      return !todo.completed || showCompleted;
+    });
+
+    filterTodos = filterTodos.filter((todo) => {
+      var text = todo.text.toLowerCase();
+      return searchText.length === 0 || text.indexOf(searchText) > -1;
+    });
+
+    filterTodos = filterTodos.sort((a, b) => {
+      if(!a.completed && b.completed) {
+        return -1;
+      }
+      else if(a.completed && !b.completed) {
+        return 1;
+      }
+      else {
+        return 0;
+      }
+
+    })
+    return filterTodos;
   }
 };
--- app/tests/api/TodoAPI.test.jsx
+++ app/tests/api/TodoAPI.test.jsx
@@ -48,4 +48,40 @@ describe('TodoAPI', () => {
       });
     });
 
+    describe('filterTodos', () => {
+        var todos = [{
+          id: 1,
+          text: 'Item 1',
+          completed: true
+        }, {
+          id: 2,
+          text: 'Item 2',
+          completed: false
+        }, {
+          id: 3,
+          text: 'Item 3',
+          completed: true
+        }];
+        it('should return all items if showCompleted is true', () => {
+          var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+          expect(filteredTodos.length).toBe(3);
+        });
+        it('should return items which are not completed if showCompleted is false', () => {
+          var filteredTodos = TodoAPI.filterTodos(todos, false, '');
+          expect(filteredTodos.length).toBe(1);
+        });
+        it('should sort items with completed status', () => {
+          var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+          expect(filteredTodos[0].completed).toBe(false);
+        });
+        it('should filter todos by searchText', () => {
+          var filteredTodos = TodoAPI.filterTodos(todos, true, '2');
+          expect(filteredTodos.length).toBe(1);
+        });
+        it('should return  all todos searchText is empty', () => {
+          var filteredTodos = TodoAPI.filterTodos(todos, true, '');
+          expect(filteredTodos.length).toBe(3);
+        });
+
+      });
 });
